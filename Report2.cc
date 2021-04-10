d47e0841d6a5bb60a19bf1df0eb3dcd386dc94ea
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-04-10 23:33:55 +0300

    Add files via upload

--- Report2.cc
+++ Report2.cc
@@ -28,7 +28,29 @@ void Report2::compute(){
 
 	//take data from the year maps and use the populate function first
 	ReportGenerator::populate();
+	//year map
+	vector<Record*>& a=yearMap[2011];
+	cout<<a.size()<<endl;
+	vector<Record*>& b=yearMap[2016];
+	cout<<b.size()<<endl;
 	
+	vector<Record*> k;
+	for(int i=0;i<a.size();i++){
+		if(a[i]->getRegion()=="CAN"){
+			k.push_back(a[i]);
+		}
+	}
+	vector<Record*> m;
+	for(int i=0;i<b.size();i++){
+		if(b[i]->getRegion()=="CAN"){
+			m.push_back(b[i]);
+		}
+	}
+	
+	cout<<k.size()<<endl;
+	for(int i=0;i<k.size();i++){
+		cout<<*k[i];
+	}
 
 }
 
