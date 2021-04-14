7ef9998f865087cc9f92eb7ebde29768c6c498f9
Jay <63338983+NirmithVictor@users.noreply.github.com>
2021-04-14 19:23:26 +0300

    Add files via upload

--- main.cc
+++ main.cc
@@ -5,38 +5,9 @@ using namespace std;
 #include "Control.h"
 
 int main(){
-	//cout<<"TEST 6"<<endl;
-	/*
 	
-	Report1* d=new Report1();
-	Report2* c=new Report2();
-	int a;
-	cin>>a;
-	Report3* b=new Report3();
-	while(a!=0){
-		if(a==2){
-		c->compute();
-		}
-		else if (a==3){
-		b->compute();
-		//delete b;
-		}
-		else if(a==1){
-		d->compute();
-		}
-		cin>>a;
-	}
-	//Report3* b=new Report3();
-	//b->compute();
-	//Report1* a=new Report1();
-	//a->compute();
-	delete b;
-	delete c;
-	delete d;*/
 	Control c;
 	c.launch();
 	return 0;
 }
 
-//	Control c;
-//	c.launch();
